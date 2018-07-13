8d27d021fcb89b90d3f716ec9c50ee0ac60f5501
Jon foster <redcabinny@gmail.com>
2018-07-12 23:06:34 -0400

    begin adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RoutingModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MusicComponent } from './music/music.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicComponent
   ],
   imports: [
     BrowserModule
+    RoutingModule.forRoot([
+    	{ path: '', component: MusicComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
